e2ac0ae36db296bbb7bc667303a8bd29ff73dae4
nicolas.bern <nicolas.bern@epsi.fr>
2020-10-28 14:29:55 +0100

    Méthodes statiques

---
+++ src/main/java/utils/ConversionNombre.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class ConversionNombre {
+
+    public static void main(String[] args) {
+        String chaine = "42";
+        int i = Integer.parseInt(chaine);
+
+        System.out.println(i);
+
+        int a=15, b=34567;
+
+        System.out.println(Integer.max(a, b));
+    }
+}
