66812065d918bd223a3be71e4082a85603661c78
nicolas.bern <nicolas.bern@epsi.fr>
2020-11-13 14:00:06 +0100

    InteractifTableMult

---
+++ src/main/java/fr/algorithmie/InteractifTableMult.java
@@ -0,0 +1,33 @@
+package fr.algorithmie;
+
+import java.util.Scanner;
+
+public class InteractifTableMult {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String str;
+        boolean isInt = false;
+        int nbr = 0;
+
+        System.out.println("Entrez un chiffre entre 1 et 10");
+        while (isInt != true){
+            str = input.nextLine();
+            try {
+                nbr = Integer.parseInt(str);
+                isInt = true;
+                while (nbr < 1 || nbr > 10){
+                    System.out.println("Entrez un chiffre entre 1 et 10");
+                    nbr = input.nextInt();
+                }
+            } catch (NumberFormatException e){
+                System.out.println("Cette valeur n'est pas un chiffre, essayez encore");
+            }
+        }
+
+        System.out.println("La table de multiplication de " + nbr + " est :");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(nbr + " x " + i + " = " + nbr*i);
+        }
+    }
+}
