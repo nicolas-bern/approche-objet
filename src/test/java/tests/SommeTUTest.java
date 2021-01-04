75df161192a0863b2cdb7e39fbb59684a7670168
nicolas.bern <nicolas.bern@epsi.fr>
2021-01-04 17:52:21 +0100

    Modifs Test unitaire

--- src/test/java/tests/SommeTUTest.java
+++ src/test/java/tests/SommeTUTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SommeTUTest extends SommeTU {
 
     @Test
-    void somme() {
+    void sommeDeuxEntiers() {
         int a = 5;
         int b = 2;
         assertEquals(7, sommeDeuxEntiers(a, b));
